bd241ba00f7eb0f9b7f1fe7a858c36c142071b30
Alena D <denyushina.alena@gmail.com>
2022-11-14 21:44:36 +0300

    add some math in code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Changes in second_branch");
+        int a=5;
+        int b=7;
+        System.out.println(a+b);
     }
 }
